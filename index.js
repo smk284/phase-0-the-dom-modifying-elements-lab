40bc6fbbb4138f3baa1fcc99b9f0adc8c1a35aa5
Shaik Kamil <shaikm.kamil@gmail.com>
2021-12-20 16:14:02 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Write your code here!
+
+main.remove();
+
+const newHeader = document.createElement("h1");
+
+newHeader.setAttribute('id','victory');
+
+newHeader.textContent =`Kamil is the champion`;
+
+document.body.append(`h1`);
+
+
+
+
+
+
